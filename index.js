7b2da2892f1566d79e5a9f7b34b04e440b1de631
Esteve2604 <estebanasperuiz@gmail.com>
2023-03-04 09:57:59 +0100

    ad

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./app') // the actual Express application
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8080, () => {
   logger.info(`Server running on port 8000`)
 })
