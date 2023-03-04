06a5616328b60604415d5fde04b419eb71943095
Esteve2604 <estebanasperuiz@gmail.com>
2023-03-04 09:56:58 +0100

    fun

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./app') // the actual Express application
 
-app.listen(3000, () => {
-  logger.info(`Server running on port 3000`)
+app.listen(8000, () => {
+  logger.info(`Server running on port 8000`)
 })
